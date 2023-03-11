017c4bb67369a267b77298833bbe158377c3579b
Ilham Manziz <imanziz850@gmail.com>
2023-03-11 19:20:07 +0700

    first commit

---
+++ resources/views/layouts/inc/kop.blade.php
@@ -0,0 +1,14 @@
+<div class="row ml-3 mr-3 mt-3">
+    <div class="col-3">
+        <img src="/adminlte/dist/img/logo.png" width="100px">
+    </div>
+    <div class="col-6 text-center">
+        <p>PEMERINTAH DAERAH PROVINSI JAWA BARAT<br>DINAS PENDIDIKAN<br>
+        <strong>SMK NEGERI 1 PADAHERANG</strong><br>
+        Jalan Raya Padaherang Km.01 (0265)655621 Desa Karangsari Kec. Padaherang <br>
+        Website : www.smkn1padaherang.sch.id E-mail : smkn_padaherang@yahoo.co.id
+        </p>
+    </div>
+</div>
+<hr style="height: 1px; border:none; color:black; background-color:black" width="97%">
+<hr style="height: 1px; border:none; color:black; background-color:black  margin-top: -20px;" width="97%">
